98bb1d764a5c13ef68f5574ce07ddc002efe9b53
objektwerks <objektwerks@runbox.com>
2023-03-09 06:41:19 -0500

    edited imports

--- src/test/scala/objektwerks/StructuredStreamingTest.scala
+++ src/test/scala/objektwerks/StructuredStreamingTest.scala
@@ -3,10 +3,10 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class StructuredStreamingTest extends AnyFunSuite with Matchers {
-  import SparkInstance._
-  import sparkSession.implicits._
+import SparkInstance._
+import sparkSession.implicits._
 
+class StructuredStreamingTest extends AnyFunSuite with Matchers {
   test("structured streaming") {
     import Person._
     sparkSession
