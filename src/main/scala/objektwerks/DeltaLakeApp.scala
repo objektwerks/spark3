5510b1442661c01e6d6be3a1908960dabf6afd64
objektwerks <objektwerks@runbox.com>
2023-03-14 16:53:07 -0400

    added data lake app

---
+++ src/main/scala/objektwerks/DeltaLakeApp.scala
@@ -0,0 +1,46 @@
+package objektwerks
+
+import SparkInstance._
+import Person.personStructType
+
+object DeltaLakeApp extends App {
+  batch()
+  structuredStreaming()
+
+  def batch(): Unit = {
+    val personsPath = "./target/delta/persons"
+    val personsDataframe = sparkSession.read.json("./data/person/person.json")
+    personsDataframe.write.format("delta").mode("overwrite").save(personsPath)
+    val personsDelta = sparkSession.read.format("delta").load(personsPath)
+    personsDelta.select("*").show
+    assert( personsDelta.select("*").count == 4 )
+  }
+
+  def structuredStreaming(): Unit = {
+    val rolesPath = "./target/delta/roles"
+    sparkSession
+      .readStream
+      .schema(personStructType)
+      .json("./data/person")
+      .groupBy("role", "name")
+      .count
+      .writeStream
+      .format("delta")
+      .outputMode("complete")
+      .option("checkpointLocation", "./target/delta/roles/checkpoints")
+      .start(rolesPath)
+      .awaitTermination(30000) // Time-dependent due to slow Delta Lake IO!
+    sparkSession
+      .readStream
+      .format("delta")
+      .load(rolesPath)
+      .writeStream
+      .format("console")
+      .outputMode("append")
+      .start
+      .awaitTermination(30000)
+    val rolesDelta = sparkSession.read.format("delta").load(rolesPath)
+    rolesDelta.select("*").show
+    assert( rolesDelta.select("*").count == 4 )
+  }
+}
