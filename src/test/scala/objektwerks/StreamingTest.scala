7848060fa7feecc59071d46b2fab9685343af4ef
objektwerks <objektwerks@runbox.com>
2023-03-15 16:59:18 -0400

    edited streaming test

--- src/test/scala/objektwerks/StreamingTest.scala
+++ src/test/scala/objektwerks/StreamingTest.scala
@@ -12,24 +12,30 @@ class StreamingTest extends AnyFunSuite with Matchers {
   test("batch") {
     val streamingContext = new StreamingContext(sparkContext, batchDuration = Milliseconds(100))
     val dstream = textToDStream("./data/txt/license.txt", streamingContext)
+    
     val wordCountDstream = countWords(dstream)
     val buffer = mutable.ArrayBuffer[(String, Int)]()
     wordCountDstream foreachRDD { rdd => buffer ++= rdd.collect }
+
     streamingContext.start
-    streamingContext.awaitTerminationOrTimeout(100)
+    streamingContext.awaitTerminationOrTimeout(1000)
     streamingContext.stop(stopSparkContext = false, stopGracefully = true)
+
     buffer.size shouldBe 96
   }
 
   test("window") {
     val streamingContext = new StreamingContext(sparkContext, batchDuration = Milliseconds(200))
     val dstream = textToDStream("./data/txt/license.txt", streamingContext)
+
     val wordCountDstream = countWords(dstream, windowLengthInMillis = 200, slideIntervalInMillis = 200)
     val buffer = mutable.ArrayBuffer[(String, Int)]()
     wordCountDstream foreachRDD { rdd => buffer ++= rdd.collect }
+
     streamingContext.start
-    streamingContext.awaitTerminationOrTimeout(100)
+    streamingContext.awaitTerminationOrTimeout(1000)
     streamingContext.stop(stopSparkContext = false, stopGracefully = true)
+
     buffer.size shouldBe 96
   }
 }
