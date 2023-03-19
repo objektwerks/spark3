b61675ada8f6a142395b6c70f9bfbe9957984036
objektwerks <objektwerks@runbox.com>
2023-03-19 08:18:21 -0400

    added window test template

---
+++ src/test/scala/objektwerks/WindowTest.scala
@@ -0,0 +1,18 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class WindowTest extends AnyFunSuite with Matchers {
+  test("tumbling") {
+
+  }
+
+  test("sliding") {
+
+  }
+
+  test("session") {
+    
+  }
+}
