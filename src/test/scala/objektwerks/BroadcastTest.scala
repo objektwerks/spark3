4a4b82952d19052c8bc94bbf8eceb48c0705d372
objektwerks <objektwerks@runbox.com>
2023-03-20 16:50:53 -0400

    enhanced broadcast test

--- src/test/scala/objektwerks/BroadcastTest.scala
+++ src/test/scala/objektwerks/BroadcastTest.scala
@@ -7,6 +7,12 @@ import SparkInstance._
 import sparkSession.implicits._
 
 class BroadcastTest extends AnyFunSuite with Matchers {
+  test("broadcast variable") {
+    val keyValues = List( KeyValue(1, 1), KeyValue(2, 2), KeyValue(3, 3) )
+    val broadcastKeyValues = sparkContext.broadcast( keyValues )
+    broadcastKeyValues.value.length shouldBe 3
+  }
+
   test("broadcast join") {
     val broadcastPersons = sparkContext.broadcast( sparkSession.read.json("./data/person/person.json").as[Person] )
 
