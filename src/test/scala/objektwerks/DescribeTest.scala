e1ef835972f5b882bbb083b0ac0f0c082578319a
objektwerks <objektwerks@runbox.com>
2023-03-20 17:51:23 -0400

    edited describe test

--- src/test/scala/objektwerks/DescribeTest.scala
+++ src/test/scala/objektwerks/DescribeTest.scala
@@ -8,8 +8,8 @@ import SparkInstance._
 import sparkSession.implicits._
   
 class DescribeTest extends AnyFunSuite with Matchers {
-  test("explain ") {
-    val persons: Dataset[Person] = sparkSession.read.json("./data/person/person.json").as[Person].cache
+  test("describe") {
+    val persons = sparkSession.read.json("./data/person/person.json").as[Person].cache
     persons.count shouldBe 4
     persons.describe("id", "age", "name", "role").show()
   }
